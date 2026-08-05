b039b4df6f00b7aa25ab529ce4b8ffa2c0ecd162
Amir Kermani <amirkermani99@gmail.com>
2026-08-05 13:39:56 +0330

    exclude modal section

--- resources/views/Horeca/index.blade.php
+++ resources/views/Horeca/index.blade.php
@@ -27,20 +27,8 @@
         </div>
     </div>
 
-    <div class="modal fade" id="imageModal" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog modal-sm modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-body text-center d-flex flex-column gap-2">
-                    <img id="modalQrImage" src="" alt="qrcode-address" class="img-fluid rounded shadow-lg">
-                    <h6 id="modalTitle" class="mt-2 mb-0"></h6>
-                    <button type="button" class="btn btn-outline-dark align-self-center btn-sm px-2 py-1 mt-2"
-                        data-bs-dismiss="modal">
-                        × بستن
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('horeca.partials.modal')
+    
     <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/leaflet/leaflet.js') }}"></script>
 
